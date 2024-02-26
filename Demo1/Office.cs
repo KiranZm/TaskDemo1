faaf1306e7e0e4eda8995a132acdfbc8cd823901
Kiran <Kiran@DESKTOP-QFFO5F8>
2024-02-26 17:05:49 +0530

    Add project files.

---
+++ Demo1/Office.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+   public class Office : ILogs
+    {
+        public void login()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void logout()
+        {
+           
+            Console.WriteLine("Hello Dear demo");
+            Console.ReadLine();
+        }
+        public static void main(string[] args)
+        {
+            Office obj = new Office();
+            obj.logout();
+        }
+       
+    }
+}
